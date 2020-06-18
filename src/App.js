b883fe411343d5b7b25d78b1167c28f90ef780d2
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-18 22:14:38 +0500

    Basic styling done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      {/* Header component start */}
+      <h1 className="text-center">Expense Tracker</h1>
+      {/* Header component End */}
+
+      {/* Balance component start */}
+      <h3>
+        your Balances <br /> $260
+      </h3>
+      {/* Balance component End */}
+
+      {/* Income expense component start */}
+      <div className="d-flex justify-content-around border bg-white">
+        <h3>
+          income <br /> $260
+        </h3>
+        <h3>
+          expense <br /> $260
+        </h3>
+      </div>
+
+      {/* Income expense component end*/}
+
+      {/* History  component start*/}
+      <h3>History</h3>
+      <hr></hr>
+      <ul className="list-group border">
+        <li className="list-group-item mb-2 d-flex justify-content-between align-items-center">
+          <span>Cash</span>
+          <span className="badge badge-primary badge-pill py-2 px-3">$500</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          <span>Cash</span>
+          <span className="badge badge-danger badge-pill py-2 px-3">$500</span>
+        </li>
+      </ul>
+
+      {/* History  component end*/}
+
+      {/* Transaction  component start*/}
+      <h3>Add new Transaction</h3>
+      <hr></hr>
+
+      <form>
+        <div className="form-group">
+          <label className="w-100">
+            Text
+            <input
+              className="form-control"
+              HtmlFor="text"
+              placeholder="Enter Text..."
+              required
+            ></input>
+          </label>
+        </div>
+
+        <div className="form-group ">
+          <label className="w-100">
+            Amount
+            <input
+              className="form-control "
+              HtmlFor="amount"
+              placeholder="Enter Amount..."
+              required
+            ></input>
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-block">
+          {" "}
+          Add Transaction{" "}
+        </button>
+      </form>
+
+      {/* Transaction  component end*/}
     </div>
   );
 }
