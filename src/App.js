d255fc4fd4c9400835590b4988a63688f1a7dca9
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-19 21:42:47 +0500

    Little issue fixed.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { TransactionContext } from "./context/transContext";
-import { TransactionProvider } from "./context/transContext";
 
 function App() {
   let { transactions } = useContext(TransactionContext);
